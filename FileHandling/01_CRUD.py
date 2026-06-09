a9f30c408e0d9f5c1fcb3ae4fefbb82338b8a34a
AkashKumarVishwakarma <akashkumargyn2002@gmail.com>
2026-06-09 17:48:48 +0530

    add file handling methods and a Project which perform CURD operation on file

---
+++ FileHandling/01_CRUD.py
@@ -0,0 +1,20 @@
+# p = open(r"C:\Users\DELL\demo\hello.txt")
+# print(p.read())
+
+p = open('FileHandling/01_CRUD.py')
+print(p.read())
+
+
+
+# r = open("fileHandling/file.txt", "w")
+# r = open("fileHandling/file.txt", "a")
+# r.write("\n Hey i am writing indside exiting file using 'a' mode ")
+
+# r.close()
+
+
+# # only for creating file run one time only "x"
+
+# r = open("fileHandling/file1.txt", "x")
+# # r.write("Hey")
+# r.close()
