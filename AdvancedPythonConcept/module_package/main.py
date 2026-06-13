5b0a77b93df00e0f26ab3f5edbd4114aa97d6fc6
AkashKumarVishwakarma <akashkumargyn2002@gmail.com>
2026-06-13 15:56:12 +0530

    Add module and package implementation and useCase

---
+++ AdvancedPythonConcept/module_package/main.py
@@ -0,0 +1,8 @@
+
+# from maths import addition
+# print(addition(12,12))
+
+
+import maths
+print(maths.addition(12,12))
+print(maths.multiplication(12,12))
---
+++ AdvancedPythonConcept/module_package/package.py
@@ -0,0 +1,12 @@
+# # from modelss import hello
+# from modelss import hello,maths1
+
+# hello.hello()
+
+# print(maths1.addition(12,2))
+
+#*************************************************************
+
+from modelss.model import hello1, division
+
+hello1.greet()
